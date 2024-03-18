13286e9cd67918e68ec16ec26d200555d3f31b3b
JoseFausti <joseemilianofausti@gmail.com>
2024-03-17 22:38:01 -0300

    new

---
+++ src/Clase_1/Persona.java
@@ -0,0 +1,43 @@
+public abstract class Persona {
+    // Atributos protegidos
+    protected int id;
+    protected int dni;
+    protected String nombreCompleto;
+
+    // Constructor
+    public Persona(int id, int dni, String nombreCompleto) {
+        this.id = id;
+        this.dni = dni;
+        this.nombreCompleto = nombreCompleto;
+    }
+
+    // Getters
+    public int getId() {
+        return id;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public String getNombreCompleto() {
+        return nombreCompleto;
+    }
+
+    // Setters
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public void setNombreCompleto(String nombreCompleto) {
+        this.nombreCompleto = nombreCompleto;
+    }
+
+    // Método abstracto
+    public abstract void Informacion();
+}
+
---
+++ src/Clase_1/Reserva.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+
+public class Reserva extends Persona {
+    // Atributo privado
+    private String reservaVuelo;
+    private ArrayList<Vuelo> vuelos; // Relacion de Agregacion de la Clase Vuelos en la Clase Reserva
+
+    // Constructor
+    public Reserva(int id, int dni, String nombreCompleto, String reservaVuelo) {
+        super(id, dni, nombreCompleto);
+        this.reservaVuelo = reservaVuelo;
+        this.vuelos = new ArrayList<>(); // Inicializar la lista de vuelos
+    }
+
+    // Getter para reservaVuelo
+    public String getReservaVuelo() {
+        return reservaVuelo;
+    }
+
+    // Setter para reservaVuelo
+    public void setReservaVuelo(String reservaVuelo) {
+        this.reservaVuelo = reservaVuelo;
+    }
+
+    // Método para agregar un vuelo a la reserva
+    public void AgregarVuelo(Vuelo vuelo) {
+        vuelos.add(vuelo); // Agregar el vuelo a la lista de vuelos
+    }
+
+
+    // Método para imprimir la información de la reserva
+    @Override
+    public void Informacion() {
+        System.out.println("Información de la reserva:");
+        System.out.println("ID: " + getId());
+        System.out.println("DNI: " + getDni());
+        System.out.println("Nombre: " + getNombreCompleto());
+        System.out.println("Reserva de vuelo: " + reservaVuelo);
+        for (Vuelo vuelo : vuelos) {
+            vuelo.Informacion(); // Imprimir información de cada vuelo
+        }
+    }
+}
