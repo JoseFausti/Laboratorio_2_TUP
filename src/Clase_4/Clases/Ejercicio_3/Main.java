799d9d5d3b910fd2d1d6d6006c4652c98354c55a
JoseFausti <joseemilianofausti@gmail.com>
2024-04-11 11:17:42 -0300

    new

--- src/Clase_4/Clases/Ejercicio_3/Main.java
+++ src/Clase_4/Clases/Ejercicio_3/Main.java
@@ -2,26 +2,30 @@ package Clase_4.Clases.Ejercicio_3;
 
 public class Main {
     public static void main(String[] args) {
-        // Crear instancia de Ciudades
-        Ciudades ciudad = new Ciudades(1000000, 12345, 1, "Ciudad Ejemplo", null);
+        // Crear instancias de Ciudades
+        Ciudades ciudad1 = new Ciudades(1000000, 12345, 1, "Ciudad A", null);
+        Ciudades ciudad2 = new Ciudades(500000, 54321, 2, "Ciudad B", null);
 
         // Crear instancias de Impuestos
         Impuesto1 impuesto1 = new Impuesto1(1000000);
-        Impuesto2 impuesto2 = new Impuesto2(1000000);
-        Impuesto3 impuesto3 = new Impuesto3(1000000);
-        Impuesto4 impuesto4 = new Impuesto4(1000000);
-        Impuesto5 impuesto5 = new Impuesto5(1000000);
+        Impuesto2 impuesto2 = new Impuesto2(2000000);
 
-        // Asignar impuestos a la ciudad
-        ciudad.setImpuesto(impuesto1);
+        // Asignar impuestos a las ciudades
+        ciudad1.setImpuesto(impuesto1);
+        ciudad2.setImpuesto(impuesto2);
 
-        // Obtener el impuesto y calcularlo
+        // Obtener y calcular el impuesto de cada ciudad
+        calcularImpuesto(ciudad1);
+        calcularImpuesto(ciudad2);
+    }
+
+    public static void calcularImpuesto(Ciudades ciudad) {
         Impuesto impuestoCiudad = ciudad.getImpuesto();
         if (impuestoCiudad != null) {
             double impuestoCalculado = impuestoCiudad.CalcularImpuesto();
-            System.out.println("El impuesto calculado para la ciudad es: " + impuestoCalculado);
+            System.out.println("El impuesto calculado para " + ciudad.getNombre() + " es: " + impuestoCalculado);
         } else {
-            System.out.println("No se ha definido un impuesto para la ciudad.");
+            System.out.println("No se ha definido un impuesto para " + ciudad.getNombre());
         }
     }
 }
