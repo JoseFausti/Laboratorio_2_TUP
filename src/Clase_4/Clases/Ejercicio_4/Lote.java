451998673427a27ca7d15f435e98f08125877342
JoseFausti <joseemilianofausti@gmail.com>
2024-04-11 12:40:13 -0300

    new

--- src/Clase_4/Clases/Ejercicio_4/Lote.java
+++ src/Clase_4/Clases/Ejercicio_4/Lote.java
@@ -1,15 +1,11 @@
 package Clase_4.Clases.Ejercicio_4;
 public class Lote {
 
-	private Minera minerales[];
+	private Mineral minerales[];
 	private TipoLote tipo;
 	public Agricultor m_Agricultor;
 
 	public Lote(){
-
-	}
-
-	public void finalize() throws Throwable {
 
 	}
 
