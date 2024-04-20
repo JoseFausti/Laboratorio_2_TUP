06e42cd9c2f335a102bdf4c5b5afe496ff9cf558
JoseFausti <joseemilianofausti@gmail.com>
2024-04-20 12:08:34 -0300

    new

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Main {
+    final Level VERBOSE = Level.forName("VERBOSE", 150);
+    private static final Logger logger = LogManager.getLogger(Main.class);
+    public static void main(String[] args) {
+        Main app = new Main();
+        if(logger.isErrorEnabled())
+            logger.error("Esto es un error");
+        logger.info("Entering application");
+        logger.debug("Esto es un debug Loggin");
+        logger.log(app.VERBOSE, "A verbose message");
+
+    }
+}
