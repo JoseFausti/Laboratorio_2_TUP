723007c59fbeb053a9a2a47bc69d81d201c4c6ea
JoseFausti <joseemilianofausti@gmail.com>
2024-05-25 15:54:25 -0300

    new

--- src/Clase_10/ProyectoJPA/src/main/java/org/example/Entidades/Persona.java
+++ src/Clase_10/ProyectoJPA/src/main/java/org/example/Entidades/Persona.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.example.Entidades;
 
 import javax.persistence.*;
 import java.io.Serializable;
--- src/Clase_10/ProyectoJPA/src/main/java/org/example/Main.java
+++ src/Clase_10/ProyectoJPA/src/main/java/org/example/Main.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import org.example.Entidades.Persona;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
